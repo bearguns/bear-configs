5fe99626adcdc9f27e07237b92cab7e0c575f294
Sean Brage <seanmbrage@me.com>
2019-07-02 12:00:30 -0600

    add fish config

---
+++ config.fish
@@ -0,0 +1,8 @@
+# Make sure Fisher is installed (fish package manager)
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+set -x PATH $HOME/.local/bin $HOME/bin /usr/local/bin $PATH
